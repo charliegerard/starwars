7f3cce92124dfacf61458a7f93cc6a587d73e8e4
Charlie gerard <annecharlotte.gerard@gmail.com>
2016-04-28 14:28:03 +1000

    check audio state before starting animation

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
 window.onload = function(){
   var sound = document.getElementById('sound');
+  var start = false;
 
-  var fadeAudio = setInterval(function () {
-    if ((sound.currentTime >= 77) && (sound.volume >= 0.1)) {
-      sound.volume -= 0.1;
+  var audioState = setInterval(function(){
+    if(sound.readyState === 4){
+      start = true;
     }
-    if ((sound.currentTime >= 10) && (sound.volume <= 0.1)) {
-      sound.volume = 0.0;
-    }
-    if (sound.volume === 0.0) {
-      clearInterval(fadeAudio);
-    }
-  }, 200);
+  }, 200)
 
-
-  var title = document.getElementById('title');
-  title.style.opacity = 1;
-  var fadeOutTitle = setTimeout(function(){
-    setInterval(function(){
-      if(title.style.opacity >= 0.1){
-        title.style.opacity -= 0.1
-      } else {
-        title.style.opacity = 0;
+  if(start === true){
+    var fadeAudio = setInterval(function () {
+      if ((sound.currentTime >= 77) && (sound.volume >= 0.1)) {
+        sound.volume -= 0.1;
       }
-    }, 10)
-  }, 4000)
+      if ((sound.currentTime >= 10) && (sound.volume <= 0.1)) {
+        sound.volume = 0.0;
+      }
+      if (sound.volume === 0.0) {
+        clearInterval(fadeAudio);
+      }
+    }, 200);
 
-  var content = document.getElementById('text')
-  var animateContent = setTimeout(function(){
-    content.className += 'animated'
-  }, 6000)
+
+    var title = document.getElementById('title');
+    title.style.opacity = 1;
+    var fadeOutTitle = setTimeout(function(){
+      setInterval(function(){
+        if(title.style.opacity >= 0.1){
+          title.style.opacity -= 0.1
+        } else {
+          title.style.opacity = 0;
+        }
+      }, 10)
+    }, 4000)
+
+    var content = document.getElementById('text')
+    var animateContent = setTimeout(function(){
+      content.className += 'animated'
+    }, 6000)
+  }
 }
