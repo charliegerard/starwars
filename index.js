157c7676928c5900cdd8308bc91d1506e4a8fd7b
Charlie gerard <annecharlotte.gerard@gmail.com>
2016-04-28 14:42:49 +1000

    fixed

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 window.onload = function(){
   var sound = document.getElementById('sound');
-  var start = false;
+  var title = document.getElementById('title');
+  title.style.opacity = 1;
+  var content = document.getElementById('text')
 
   var audioState = setInterval(function(){
     if(sound.readyState === 4){
-      start = true;
+      fadeAudio();
+      fadeOutTitle();
+      animateContent();
+      clearInterval(audioState)
     }
   }, 200)
 
-  if(start === true){
-    var fadeAudio = setInterval(function () {
+  var fadeAudio = function(){
+    setInterval(function () {
       if ((sound.currentTime >= 77) && (sound.volume >= 0.1)) {
         sound.volume -= 0.1;
       }
@@ -20,10 +25,10 @@ window.onload = function(){
         clearInterval(fadeAudio);
       }
     }, 200);
+  }
 
-    var title = document.getElementById('title');
-    title.style.opacity = 1;
-    var fadeOutTitle = setTimeout(function(){
+  var fadeOutTitle = function(){
+    setTimeout(function(){
       setInterval(function(){
         if(title.style.opacity >= 0.1){
           title.style.opacity -= 0.1
@@ -32,10 +37,12 @@ window.onload = function(){
         }
       }, 10)
     }, 4000)
+  }
 
-    var content = document.getElementById('text')
-    var animateContent = setTimeout(function(){
+  var animateContent = function(){
+    setTimeout(function(){
       content.className += 'animated'
     }, 6000)
   }
+
 }
