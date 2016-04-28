b018cfef39d5dc3a54e8c8c134cdae8a93b5270e
Charlie gerard <annecharlotte.gerard@gmail.com>
2016-04-28 13:08:31 +1000

    add sound + fade out

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-//This is not needed if we remove overflow: hidden in CSS
-// window.onload = function(){
-//   var animation = document.getElementsByClassName('content')[0]
-//   document.body.appendChild(animation);
-// }
+window.onload = function(){
+  var sound = document.getElementById('sound');
+
+  var fadeAudio = setInterval(function () {
+        if ((sound.currentTime >= 70) && (sound.volume >= 0.1)) {
+            sound.volume -= 0.1;
+        }
+
+        if ((sound.currentTime >= 10) && (sound.volume <= 0.1)) {
+            sound.volume = 0.0;
+        }
+        if (sound.volume === 0.0) {
+            clearInterval(fadeAudio);
+        }
+    }, 200);
+}
