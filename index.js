119f7e9e48661ba8ea32f5412fba8e4c62c0ed25
Charlie gerard <annecharlotte.gerard@gmail.com>
2016-04-28 13:36:31 +1000

    add intro text and animations

--- index.js
+++ index.js
@@ -2,15 +2,32 @@ window.onload = function(){
   var sound = document.getElementById('sound');
 
   var fadeAudio = setInterval(function () {
-        if ((sound.currentTime >= 70) && (sound.volume >= 0.1)) {
-            sound.volume -= 0.1;
-        }
+    if ((sound.currentTime >= 70) && (sound.volume >= 0.1)) {
+      sound.volume -= 0.1;
+    }
+    if ((sound.currentTime >= 10) && (sound.volume <= 0.1)) {
+      sound.volume = 0.0;
+    }
+    if (sound.volume === 0.0) {
+      clearInterval(fadeAudio);
+    }
+  }, 200);
 
-        if ((sound.currentTime >= 10) && (sound.volume <= 0.1)) {
-            sound.volume = 0.0;
-        }
-        if (sound.volume === 0.0) {
-            clearInterval(fadeAudio);
-        }
-    }, 200);
+
+  var title = document.getElementById('title');
+  title.style.opacity = 1;
+  var fadeOutTitle = setTimeout(function(){
+    setInterval(function(){
+      if(title.style.opacity >= 0.1){
+        title.style.opacity -= 0.1
+      } else {
+        title.style.opacity = 0;
+      }
+    }, 10)
+  }, 4000)
+
+  var content = document.getElementById('text')
+  var animateContent = setTimeout(function(){
+    content.className += 'animated'
+  }, 6000)
 }
